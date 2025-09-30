0d6a1a52d4cb2e357db46a9b678315b92160a753
Josh <joshua.mwakio@gmail.com>
2025-09-30 14:54:07 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+
+    // Apply the Application plugin to add support for building an executable JVM application.
+    application
+}
+
+dependencies {
+    // Project "app" depends on project "utils". (Project paths are separated with ":", so ":utils" refers to the top-level "utils" project.)
+    implementation(project(":utils"))
+
+    // RxJava Core
+    implementation("io.reactivex.rxjava3:rxjava:3.1.12")
+    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
+
+    // Http Client
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.okhttp3:okhttp:5.1.0")
+
+    //JSON processing
+    implementation("com.google.code.gson:gson:2.13.2")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+
+    //Testing
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.20.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:6.0.0")
+
+    //For coroutines interop
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3:1.10.2")
+
+}
+
+application {
+    // Define the Fully Qualified Name for the application main class
+    // (Note that Kotlin compiles `App.kt` to a class with FQN `com.example.app.AppKt`.)
+    mainClass = "org.rx.app.AppKt"
+}
